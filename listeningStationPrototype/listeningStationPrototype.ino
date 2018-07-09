034974dffd5181d6011a502d1632c6dbb5c8155f
Jiffer <jifferh@gmail.com>
2018-07-09 17:01:47 -0600

    added folder identification

--- listeningStationPrototype/listeningStationPrototype.ino
+++ listeningStationPrototype/listeningStationPrototype.ino
@@ -29,12 +29,11 @@ int buttonPin = 32;
 boolean okToPlay = false;
 File root;
 
-char fileList[][13] = {"Bobby.wav", "Chicano.wav", "Solivan.wav", "StCanRan.wav", "YoLaTeng.wav"};
-char fileNameList[100][16]; // max of 100 tracks
+char fileNameList[100][16]; // sets a max of 100 tracks
 char dirNameList[100][16];
-char fList[100][32];
-int numTracks = 0;
-int track = 0;
+char fList[8][100][32]; // file location including directory name and filename.wav
+int numTracks[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+int currentTrack[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 void setup() {
   Serial.begin(9600);
 
@@ -59,9 +58,12 @@ void setup() {
   }
   root = SD.open("/");
 
-  printDirectory(root, 0);
+  readDirectory(root, 0, -1);
 
-  Serial.println("done!");
+  Serial.println("all done with setup!");
+
+  // configure pins
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void playFile(const char *filename)
@@ -76,15 +78,16 @@ void playFile(const char *filename)
   // A brief delay for the library read WAV info
   delay(5);
 
-  // Simply wait for the file to finish playing.
-//  while (playWav1.isPlaying()) {
-//    // uncomment these lines if you audio shield
-//    // has the optional volume pot soldered
-//    //float vol = analogRead(15);
-//    //vol = vol / 1024;
-//    // sgtl5000_1.volume(vol);
-//  }
-  pinMode(buttonPin, INPUT_PULLUP);
+  // uncomment to lock out and wait for the file to finish playing.
+  //  while (playWav1.isPlaying()) {
+  //    // uncomment these lines if you audio shield
+  //    // has the optional volume pot soldered
+  //    //float vol = analogRead(15);
+  //    //vol = vol / 1024;
+  //    // sgtl5000_1.volume(vol);
+  //  }
+
+  // how long to wait until buttons become active again:
   delay(1000);
 }
 
@@ -92,30 +95,31 @@ void playFile(const char *filename)
 void loop() {
 
   if (digitalRead(buttonPin) == 0) {
-    // concatenate folder name and file name to play file
+    int folder = 0;
     Serial.print("playing track: ");
-    Serial.println(track);
-    //    playFile(strcat(strcat(dirNameList[track], "/"), fileNameList[track]));  // filenames are always uppercase 8.3 format
-    playFile(fList[track]);
-    track++;
-    track %= numTracks;
+    Serial.println(currentTrack[folder]); // track number
+
+    playFile(fList[folder][currentTrack[folder]]);
+    currentTrack[folder]++;
+    currentTrack[folder] %= numTracks[folder];
     delay(500);
-
   }
-
 }
 
-
-void printDirectory(File dir, int numTabs) {
+void readDirectory(File dir, int numTabs, int currentPassIn) {
+  int currentFolder = currentPassIn;
   char* entryName;
-  char* lastDirName;  // = (char*)malloc(32*sizeof(char)); 
+  char* lastDirName;// = (char*)malloc(32*sizeof(char));
   while (true) {
 
     File entry =  dir.openNextFile();
     if (! entry) {
       // no more files
-      Serial.print("found this many valid .wav files: ");
-      Serial.println(numTracks);
+      if (currentFolder != -1) {
+        Serial.print("found this many valid .wav files: ");
+        Serial.println(numTracks[currentFolder]);
+      }
+      //      free(lastDirName);
       break;
     }
     for (uint8_t i = 0; i < numTabs; i++) {
@@ -136,36 +140,67 @@ void printDirectory(File dir, int numTabs) {
       }
       if (startingAt > 0) {
         if (entryName[startingAt + 1] == 'W' && entryName[startingAt + 2] == 'A' && entryName[startingAt + 3] == 'V') {
-//          char* dirName;
+          //          char* dirName;
           // put it in an array!
           for (int i = 0; i < 16; i++) {
-            dirNameList[numTracks][i] = lastDirName[i];
+            dirNameList[numTracks[currentFolder]][i] = lastDirName[i];
           }
           for (int i = 0; i < 16; i++) {
-            fileNameList[numTracks][i] = entryName[i];
+            fileNameList[numTracks[currentFolder]][i] = entryName[i];
 
           }
-          char* dirName = strcat(strcat(dirNameList[numTracks], "/"), fileNameList[numTracks]);
-          for (int i = 0; i < 32; i++) {
-//            fileNameList[numTracks][i] = entryName[i];
-            fList[numTracks][i] = dirName[i];
+
+
+
+          Serial.print(" cf is : ");
+          Serial.println(currentFolder);
+          if (currentFolder != -1) {
+            Serial.println("current folder is aewersomes");
+            char* dirName = strcat(strcat(dirNameList[numTracks[currentFolder]], "/"), fileNameList[numTracks[currentFolder]]);
+            for (int i = 0; i < 32; i++) {
+              fList[currentFolder][numTracks[currentFolder]][i] = dirName[i]; // todo! use folder name
+            }
+
+            Serial.print("stored file name: ");
+            Serial.print(fileNameList[numTracks[currentFolder]]);
+            Serial.print(" at loc: ");
+            Serial.print(numTracks[currentFolder]);
+            Serial.print(" whole ");
+            Serial.println( fList[currentFolder][numTracks[currentFolder]] );
+            numTracks[currentFolder]++;
           }
-          
-          Serial.print("stored file name: ");
-          Serial.print(fileNameList[numTracks]);
-          Serial.print(" at loc: ");
-          Serial.print(numTracks);
-          Serial.print(" whole ");
-          Serial.println( fList[numTracks] );
-          numTracks++;
         }
       }
       Serial.print(entry.name());
 
       if (entry.isDirectory()) {
         lastDirName = entry.name();
+        currentFolder = -1;
+        if (lastDirName[0] == 'A' ) {
+          Serial.println("current folder is 0");
+          currentFolder = 0;
+        } else if (lastDirName[0] == 'B' ) {
+          Serial.println("current folder is 1");
+          currentFolder = 1;
+        } else if (lastDirName[0] == 'C' ) {
+          Serial.println("current folder is 2");
+          currentFolder = 2;
+        } else if (lastDirName[0] == 'D' ) {
+          Serial.println("current folder is 3");
+          currentFolder = 3;
+        } else if (lastDirName[0] == 'E' ) {
+          currentFolder = 4;
+        } else if (lastDirName[0] == 'F' ) {
+          currentFolder = 5;
+        } else if (lastDirName[0] == 'G' ) {
+          currentFolder = 6;
+        } else if (lastDirName[0] == 'H' ) {
+          currentFolder = 7;
+        }
+
+
         Serial.println("/");
-        printDirectory(entry, numTabs + 1);
+        readDirectory(entry, numTabs + 1, currentFolder);
       } else {
         // files have sizes, directories do not
         Serial.print("\t\t");
